1caab67610357427dcc65d2305a46224d27772be
shuhai <admin@4wei.cn>
2017-05-27 15:57:49 +0800

    fix some logic

--- bootstrap.php
+++ bootstrap.php
@@ -6,12 +6,15 @@
     require_once('../config.php');
     
     # ENABLE DEBUG
-    if(PSM_DEBUG) {
+    if(defined('PSM_DEBUG')) {
         ini_set('display_errors', 'On');
         error_reporting(E_ALL);
     }
 
     # LOAD CLASSES
+    if (!is_file('vendor/autoload.php')) {
+        die('{"tag":"login","success":0,"error_msg":"Run composer install first."}');
+    }
     require_once('vendor/autoload.php');
 
     # LOAD ELOQUENT
